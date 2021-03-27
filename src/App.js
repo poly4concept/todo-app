ccf7df8db996c5287d5abc7db45ae8b54b355336
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-03-27 15:54:18 +0100

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>Todo App</h1>
       <Form  addTask={addTask} />
       <div className="filters btn-group stack-exception">
       {filterList}
